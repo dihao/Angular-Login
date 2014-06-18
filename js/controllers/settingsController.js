d4b0892111486c98add751d5a04a9c7a478581bd
alan <alandunnen@icloud.com>
2014-06-18 16:11:32 +0100

    Commit 19 - After revert
    This commit is a reverted copy of commit 17 after losing data.

--- js/controllers/settingsController.js
+++ js/controllers/settingsController.js
@@ -10,53 +10,72 @@ loginApp.controller('SettingsController', ['$scope', '$http', '$cookies', 'Login
 	// Update email address.
 	$scope.settingsEmail = function(){
 		if($scope.email_form.$valid){
-			$http({
-				method: 'PATCH',
-				url: 'https://localhost:3000/userAccounts/profileUtilities/changeEmailAddress',
-				data: $.param($scope.change)
-			}).success(function(data){
-				$scope.successEmailAddressChange = 'Your Email has been changed';
-				$scope.sameEmailAddressError = '';
-				$timeout(function() {
-					LoggedInUserFactory.setUser(angular.fromJson($cookies.userInfoCookie));
-					ProfileFactory.setUserProfile(angular.fromJson($cookies.userInfoCookie));
-				}, 100);
-			}).error(function(error, status){
+			if($scope.user.emailAddress != $scope.change.emailAddress){
+				$http({
+					method: 'PATCH',
+					url: 'https://localhost:3000/userAccounts/profileUtilities/changeEmailAddress',
+					data: $.param($scope.change)
+				}).success(function(data){
+					$scope.successEmailAddressChange = 'Your Email has been changed';
+					$scope.sameEmailAddressError = '';
+					$timeout(function() {
+						LoggedInUserFactory.setUser(angular.fromJson($cookies.userInfoCookie));
+						ProfileFactory.setUserProfile(angular.fromJson($cookies.userInfoCookie));
+					}, 100);
+				}).error(function(error, status){
+					$scope.sameEmailAddressError = 'That is the email you are currently using';
+					$scope.successEmailAddressChange = '';
+					console.log(error, status, 'error');
+				});	
+			}else{
 				$scope.sameEmailAddressError = 'That is the email you are currently using';
 				$scope.successEmailAddressChange = '';
-				console.log(error, status, 'error');
-			});
-			/*
-			if($scope.user.emailAddress != $scope.change.emailAddress){
-				$scope.user.emailAddress = $scope.change.emailAddress; // Set the users email to the new email
-				$scope.successEmailAddressChange = 'Your Email has been changed';
-			}else{ // Else the emails are the same.
-				$scope.sameEmailAddressError = 'That is the email you are currently using';
 			}
-			*/
 		}else{
 			$scope.email_form.submitted = true;
 		}
 	};
-
+	
+	
+	
+	
 	// Update Password.
 	$scope.settingsPassword = function(){
 		if($scope.password_form.$valid){
+  			$http({
+  				method: 'PATCH',			
+  				url: 'https://localhost:3000/userAccount/profileUtilities/changePassword',
+  				data: $.param($scope.change)
+  			}).success(function(data){
+	  			$scope.successPassChange = 'Your Password has been changed';
+	  			$scope.confirmPasswordError = '';
+	  			$scope.successDelete = 'Your Password has been changed';
+	  			console.log('success');
+  			}).error(function(error, status){
+  				$scope.confirmPasswordError = 'Your passwords do not match';
+  				$scope.successPassChange = '';
+  				console.log(error, status, 'error');
+  			});
+  		}else{
+  			$scope.password_form.submitted = true;
+  		}
+  	};
+
+	// Delete User.
+	$scope.settingsDelete = function(){
+		if($scope.delete_form.$valid){
 			$http({
 				method: 'PATCH',
-				url: 'https://localhost:3000/userAccount/profileUtilities/changePassword',
+				url: 'https://localhost:3000/userAccount/profileUtilities/closeAccount',
 				data: $.param($scope.change)
 			}).success(function(data){
-				$scope.successPassChange = 'Your Password has been changed';
-				$scope.confirmPasswordError = '';
+				$scope.successDelete = 'Your Password has been changed';
 				console.log('success');
 			}).error(function(error, status){
-				$scope.confirmPasswordError = 'Your passwords do not match';
-				$scope.successPassChange = '';
 				console.log(error, status, 'error');
 			});
 		}else{
-			$scope.password_form.submitted = true;
+			$scope.delete_form.submitted = true;
 		}
 	};
 
